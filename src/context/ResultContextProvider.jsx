e626ee3db5de7683ce75ca2226485168097abe8e
Arijit <saha65092@gmail.com>
2024-08-12 23:41:32 +0530

    added context folder

---
+++ src/context/ResultContextProvider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import React, { createContext, useContext, useState } from 'react';
+
+function ResultContextProvider() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ResultContextProvider
